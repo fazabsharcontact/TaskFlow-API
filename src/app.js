7e7cc7acf63a50ec865f6aa46a23adf25c706714
fazaabsharcontact <fazaabsharcontact@gmail.com>
2026-08-22 19:25:16 +0700

    adding route get/about and /health

--- src/app.js
+++ src/app.js
@@ -6,4 +6,19 @@ app.get("/", (req, res) => {
     res.send('taskflow is running...');
 });
 
+app.get("/about", (req, res) => {
+    res.json({
+        name: "TaskFlow API",
+        version: "1.0.0",
+        description: "task management REST API"
+    });
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ 
+        status: "ok",
+        message: "Server is healthy" 
+    });
+});
+
 export default app;
